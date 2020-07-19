46e40b0acd0fe35b3b0cafc68c8740014d3910fe
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-18 23:12:07 -0400

    Fetch news. Handle fetch errors

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
+import Error from 'next/error'
+
 class Index extends React.Component {
 
-  render () {
-    return (
-      <div> Techie Rumor </div>
-    )
-  }
+    static async getInitialProps() {
+
+        let stories;
+
+        try {
+            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            stories = await response.json();
+        } catch (err) {
+            console.log(err);
+            stories = [];
+        }
+
+        return {stories};
+    }
+
+    render() {
+
+        const {stories} = this.props;
+
+        if (stories.length === 0) {
+            return <Error statusCode={503}/>
+        }
+
+        return (
+            <div>
+                <h1>
+                    Techie Rumor
+                </h1>
+                <div>
+                    {
+                        stories.map(story => (<h2>
+                            {story.title}
+                        </h2>))
+                    }
+                </div>
+            </div>
+        )
+
+    }
 }
 
 export default Index;
