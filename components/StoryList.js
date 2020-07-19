4c9e9793f5de86c0b2c1ca551a91d5548df9609b
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-19 00:52:01 -0400

    Add styles to news stories

--- components/StoryList.js
+++ components/StoryList.js
@@ -23,6 +23,54 @@ const StoryList = ({stories}) => (
                 </div>
             ))
         }
+
+        <style jsx> 
+            {`
+            
+            .story-list {
+                padding: 0 1em;
+
+            }
+
+            .story {
+                padding: 1em 0;
+            }
+
+            .story-title {
+                font-size: 1rem;
+                font-weight: 400;
+                margin: 0;
+                margin-bottom: 0.5em;
+            }
+
+            .story-title a {
+                color: #333;
+                text-decoration: none;
+            }
+
+            .story-title a:hover, .story-details a:hover {
+                text-decoration: underline;
+
+            }
+
+            .story-details {
+                font-size: 0.8rem;
+                font-weight: bold;
+            }
+
+            .story-details span {
+            margin-right: 1em;
+
+            }
+
+            .story-details a {
+                color: #6600ff;
+                text-decoration: none;
+
+            }
+
+            `}
+        </style>
     </div>
 )
 
