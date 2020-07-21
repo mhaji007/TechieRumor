f30aa6c0456a91c9b3374b32837b5abefd31b874
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-21 19:21:36 -0400

    Fetch next page of stories. Add pagination using query parameters

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,32 @@
 import Error from 'next/error';
+import Link from 'next/link';
 import StoryList from '../Components/StoryList';
 import Layout from '../components/Layout';
 
 class Index extends React.Component {
 
-    static async getInitialProps() {
+    static async getInitialProps({req, res, query}) {
+        
 
         let stories;
+        let page;
 
         try {
-            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            page= Number(query.page) || 1;
+            console.log(page);
+            const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
             stories = await response.json();
         } catch (err) {
             console.log(err);
             stories = [];
         }
 
-        return {stories};
+        return {stories, page};
     }
 
     render() {
 
-        const {stories} = this.props;
+        const {stories, page} = this.props;
 
         if (stories.length === 0) {
             return <Error statusCode={503}/>
@@ -32,6 +37,31 @@ class Index extends React.Component {
             Hacker News built with Next.js">
 
                 <StoryList stories = {stories}/>
+
+                <footer>
+                    <Link href={`/?page=${page+1}`}>
+                        <a>Next Page ({page + 1}) </a>
+                    </Link>
+                </footer>
+
+                <style jsx>
+                {`
+                    footer {
+                        padding: 1em;
+                    }
+                    
+                    footer a {
+
+                        font-weight: bold;
+                        color:black;
+                        text-decoration:none;
+
+                    }
+                
+                
+                `}
+
+                </style>
                 
             </Layout>
         )
